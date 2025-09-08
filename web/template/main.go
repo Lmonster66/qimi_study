1e53964821d60a449852aa6c1e9827f074de734f
zheng.xiang <zheng.xiang@mxi.ai>
2025-09-08 16:26:35 +0800

    新增嵌套模板

--- web/template/main.go
+++ web/template/main.go
@@ -29,8 +29,23 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, user)
 }
 
+func tmplDemo(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./t.tmpl", "./ul.tmpl")
+	if err != nil {
+		fmt.Println("create template failed, err:", err)
+		return
+	}
+	user := UserInfo{
+		Name:   "小王子",
+		Gender: "男",
+		Age:    18,
+	}
+	tmpl.Execute(w, user)
+}
+
 func main() {
-	http.HandleFunc("/", sayHello)
+	//http.HandleFunc("/", tmplDemo)
+	http.HandleFunc("/", tmplDemo)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("HTTP server failed,err:", err)
